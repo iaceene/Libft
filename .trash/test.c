f2b35f394038baa469a1f2e0c9c0b85ad31ba5a4
Yassine <yassineajagrou986@gmail.com>
2024-08-22 02:26:01 +0100

    size t

--- .trash/test.c
+++ .trash/test.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 
-// Custom implementation of isdigit
-int my_isdigit(int c) {
-    if (c >= '0' && c <= '9')
-        return (1);
-    return (0);
-}
+int main(void) {
+	int a = 5;	// declaring an integer variable and assigning the value of 5
+	int *ptr;	// declaring a pointer to integer
+	int b;		// declaring an integer variable
+    printf("ptr's value: %2d, ptr's address: %p\n\n", *ptr, ptr);
 
-int main() {
-    char testChar = 'A'; // Non-digit character
-
-    if (!my_isdigit(testChar)) {
-        printf("%c is not a digit.\n", testChar);
-    } else {
-        printf("%c is a digit.\n", testChar);
-    }
-
-    return 0;
+	ptr = &a;	// pointer ptr points to what is stored at the memory address of variable a
+	b = a;		// b will take the value and not the address
+	a = 42;		// b is still equal to 5, but ptr will return 42, which is the value now stored at a's location;
+	printf("  a's value: %2d,   a's address: %p\n", a, &a);
+	printf("ptr's value: %2d, ptr's address: %p\n", *ptr, ptr); // you will get the same as above, notice that you have to dereference the pointer with * to get the value, and using the pointer alone (ptr) will give you the memory address.
+	printf("  b's value: %2d,   b's address: %p\n", b, &b);
+	//printf("Size of ptr: %zu\n", sizeof(ptr)); // size of ptr in bytes, 8 on my system.
+	return 0;
 }
--- src/ft_strlen.c
+++ src/ft_strlen.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-size_t ft_strlen(const char *s)
+size_t  ft_strlen(const char *s)
 {
-    int i;
+    size_t i;
 
     i = 0;
     while(s[i])
