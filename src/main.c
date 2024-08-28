2f2e3d48748aae9dd030482294d9f6e2493f29a6
SegFault777 <yassineajagrou986@gmail.com>
2024-08-28 22:13:16 +0000

    fix

--- src/main.c
+++ src/main.c
@@ -12,6 +12,7 @@ int main(int c, char **v)
 	//printf("%s\n", dst);
     //printf(" = %s\n", ft_strchr(src, 't' + 256));
 	//printf(" = %s\n", strchr(src, 't' + 256));
+    printf("%d", ft_isascii(src[1]));
 
 
     printf("---> success ...\n");
