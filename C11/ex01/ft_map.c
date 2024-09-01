9b805eb93501d4d256aaaecb5c2e2457249e936c
SegFault777 <yassineajagrou986@gmail.com>
2024-09-01 19:31:37 +0000

    c11

---
+++ C11/ex01/ft_map.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+int *ft_map(int *tab, int length, int(*f)(int))
+{
+    int i;
+    int *ret;
+
+    i = 0;
+    ret = malloc(sizeof(int) * length);
+    while(i < length)
+    {
+        ret[i] = f(tab[i]);
+        i++;
+    }
+    return ret;
+}
