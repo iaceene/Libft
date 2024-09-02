497b782d07f9fa9ca793e1ebfa2a9315ae84d372
SegFault777 <yassineajagrou986@gmail.com>
2024-09-02 00:48:50 +0000

    c11

---
+++ C11/ex06/ft_sort_string_tab.c
@@ -0,0 +1,47 @@
+void ft_swap(char **a, char **b)
+{
+    char *tmp;
+
+    tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+int cmp(char *str1, char *str2)
+{
+    int i;
+
+    i = 0;
+    while(str1[i] && str2[i])
+    {
+        if(str1[i] > str2[i])
+            return 1;
+        i++;
+    }
+    return 0;
+}
+
+int ft_size(char **tab)
+{
+    int i;
+
+    i = 0;
+    while(tab[i])
+        i++;
+    return i;
+}
+
+void ft_sort_string_tab(char **tab)
+{
+    int i;
+    int size;
+
+    i = 1;
+    size = ft_size(tab);
+    while(i < size - 1)
+    {
+        if(cmp(tab[i], tab[i + 1]))
+            ft_swap(&tab[i], &tab[i + 1]);
+        i++;
+    }
+}
---
+++ C11/ex07/ft_advanced_sort_string_tab.c
@@ -0,0 +1,33 @@
+void ft_swap(char **a, char **b)
+{
+    char *tmp;
+
+    tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+int ft_size(char **tab)
+{
+    int i;
+
+    i = 0;
+    while(tab[i])
+        i++;
+    return i;
+}
+
+void ft_advanced_sort_string_tab(char **tab, int(*cmp)(char *, char *))
+{
+    int i;
+    int size;
+
+    i = 1;
+    size = ft_size(tab);
+    while(i < size - 1)
+    {
+        if(cmp(tab[i], tab[i + 1]))
+            ft_swap(&tab[i], &tab[i + 1]);
+        i++;
+    }
+}
