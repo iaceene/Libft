dba4952ca14dfa02914b704b8d3805ef786d1612
Yassine Ajagrou <yassine@yassine.con>
2024-09-09 19:32:33 +0100

    adding ft_calloc

--- src/libft.h
+++ src/libft.h
@@ -34,7 +34,7 @@ void    ft_striteri(char *s, void (*f)(unsigned int, char*));
 void    ft_putchar_fd(char c, int fd);
 void    ft_putendl_fd(char *s, int fd);
 void    ft_putnbr_fd(int n, int fd);
-
+void	*ft_calloc(size_t nmemb, size_t size);
 
 
 
